a9fa08fad1b42517051e1e8edb3969d4a5e0de52
Julián Cipagauta <julian.cipa96@outlook.com>
2020-07-24 23:44:35 -0500

    Se agrego unos componentes y arreglo los scripts para heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,51 @@
 import React from 'react'
-import { Button } from 'antd'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Home } from './Pages/Home'
 import './App.less'
+import { NavBar } from './Components/Molecular/NavBar'
+import { Layout, Breadcrumb, Card } from 'antd'
+
+const { Header, Content, Footer, Sider } = Layout
 
 function App () {
   return (
-    <div className='App'>
-      <Button type='primary'>Button</Button>
-      <Button type='danger'>Button</Button>
-    </div>
+    <Router>
+      <Layout className='layout'>
+        <Sider
+          style={{
+            overflow: 'auto',
+            height: '100vh',
+            position: 'fixed',
+            left: 0
+          }}
+        />
+        <Layout style={{ marginLeft: 200 }}>
+          <Header style={{ padding: 0 }}>
+            <NavBar />
+          </Header>
+          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Card>
+              <Switch>
+                <Route path='/'>
+                  <Home />
+                </Route>
+              </Switch>
+            </Card>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    </Router>
   )
 }
+
+App.prototypes = {}
+
+App.defaultProps = {}
 
 export default App
